a7c2188a7ade13b06c82c255298c3ca7b6fd30a8
chenkai <orochi114@163.com>
2020-05-24 22:57:53 +0800

    更新图标、名称，更新样式。

--- TOTP/CloudDataManager.swift
+++ TOTP/CloudDataManager.swift
@@ -17,6 +17,18 @@ class CloudDataManager {
         static let localDocumentsURL = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: .userDomainMask).last!
         
         static let iCloudDocumentsURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents")
+
+        static func tmpDocumentsURL() -> URL {
+            let tmpDir = URL(fileURLWithPath: NSTemporaryDirectory())
+            if !FileManager.default.fileExists(atPath: tmpDir.absoluteString) {
+                do {
+                    try FileManager.default.createDirectory(at: tmpDir, withIntermediateDirectories: true, attributes: nil)
+                } catch let error as NSError {
+                    print("failed to create tmp dir", error)
+                }
+            }
+            return tmpDir
+        }
     }
     
     
@@ -53,7 +65,20 @@ class CloudDataManager {
             try? fileManager.removeItem(at: filePath)
         }
     }
-    
+    func deleteFilesInDirectory(url: URL?) {
+        let fileManager = FileManager.default
+        let enumerator = fileManager.enumerator(atPath: url!.path)
+        while let file = enumerator?.nextObject() as? String {
+
+            do {
+                try fileManager.removeItem(at: url!.appendingPathComponent(file))
+                print("Files deleted", file)
+            } catch let error as NSError {
+                print("Failed deleting files : \(error)")
+            }
+        }
+    }
+
     // Copy local files to iCloud
     // iCloud will be cleared before any operation
     // No data merging
@@ -64,7 +89,29 @@ class CloudDataManager {
             let enumerator = fileManager.enumerator(atPath: DocumentsDirectory.localDocumentsURL.path)
             while let file = enumerator?.nextObject() as? String {
                 do {
-                    try fileManager.copyItem(at: DocumentsDirectory.localDocumentsURL.appendingPathComponent(file), to: DocumentsDirectory.iCloudDocumentsURL!.appendingPathComponent(file))
+                    let sourceFile = DocumentsDirectory.localDocumentsURL.appendingPathComponent(file)
+                    let tmpFile = DocumentsDirectory.tmpDocumentsURL().appendingPathComponent(String(file.split(separator: "/").last!))
+                    let destFile = DocumentsDirectory.iCloudDocumentsURL!.appendingPathComponent(file)
+                    var isDir:ObjCBool = false
+                    if fileManager.fileExists(atPath: tmpFile.absoluteString, isDirectory: &isDir) {
+                        try fileManager.removeItem(at: tmpFile)
+                    }
+                    //try fileManager.copyItem(at: sourceFile, to: tmpFile)
+                    //try fileManager.replaceItemAt(destFile, withItemAt: tmpFile)
+                    isDir = false
+                    if fileManager.fileExists(atPath: sourceFile.absoluteString, isDirectory: &isDir) {
+                        if isDir.boolValue {
+                            try fileManager.createDirectory(at: destFile, withIntermediateDirectories: true, attributes: nil)
+                        } else {
+                            if !fileManager.fileExists(atPath: destFile.absoluteString, isDirectory: &isDir) {
+                                try fileManager.copyItem(at: sourceFile, to: destFile)
+                            } else {
+                                try fileManager.copyItem(at: sourceFile, to: tmpFile)
+                                let _ = try fileManager.replaceItemAt(destFile, withItemAt: tmpFile)
+                            }
+                        }
+                    }
+                    try fileManager.copyItem(at: sourceFile, to: destFile)
                 } catch let error as NSError {
                     print("Failed to move file to Cloud : \(error)")
                 }
@@ -85,7 +132,16 @@ class CloudDataManager {
             while let file = enumerator?.nextObject() as? String {
                 
                 do {
-                    try fileManager.copyItem(at: DocumentsDirectory.iCloudDocumentsURL!.appendingPathComponent(file), to: DocumentsDirectory.localDocumentsURL.appendingPathComponent(file))
+                    let sourceFile = DocumentsDirectory.iCloudDocumentsURL!.appendingPathComponent(file)
+                    print(file, sourceFile)
+                    let tmpFile = DocumentsDirectory.tmpDocumentsURL().appendingPathComponent(String(file.split(separator: "/").last!))
+                    let destFile = DocumentsDirectory.localDocumentsURL.appendingPathComponent(file)
+                    //var isDir:ObjCBool = false
+                    //if fileManager.fileExists(atPath: tmpFile.absoluteString, isDirectory: &isDir) {
+                    //    try fileManager.removeItem(at: tmpFile)
+                    //}
+                    try fileManager.copyItem(at: sourceFile, to: tmpFile)
+                    let _ = try fileManager.replaceItemAt(destFile, withItemAt: tmpFile)
                     print("Moved to local dir", file)
                 } catch let error as NSError {
                     print("Failed to move file to local dir : \(error)")
